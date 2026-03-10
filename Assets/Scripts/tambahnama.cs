5aa12fe112e2c803c88bf2b17221394d58555ad6
Al-Fatih123 <04alfatihdiarhanif@gmail.com>
2026-03-10 10:44:10 +0700

    tambah script buat nama revisi

--- Assets/Scripts/tambahnama.cs
+++ Assets/Scripts/tambahnama.cs
@@ -5,7 +5,7 @@ public class tambahnama : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        debug.Log("Nama saya adalah " + PlayerPrefs.GetString("nama"));
+        Debug.Log("Nama saya adalah Fatih");
     }
 
     // Update is called once per frame

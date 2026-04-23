178f3ee28daa813b4bd605eaf3b3ee1f8cbbf5fb
jeremygracecielo-cmyk <jeremygracecielo@gmail.com>
2026-04-23 15:57:34 +0700

    fix gamestate implementasi pause dan resume

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
@@ -8,31 +9,53 @@ public class GameManager : MonoBehaviour
 
     void Awake()
     {
-        Instance = this;
+       if (Instance == null) Instance = this;
+        else Destroy(gameObject);
     }
 
     void Start()
     {
-        currentState = GameState.Playing;
+        UpdateState(GameState.Playing);
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+       if (Input.GetKeyDown(KeyCode.Escape))
         {
-            PauseGame();
+            if (currentState == GameState.Playing) 
+
+            UpdateState(GameState.Pause);
+            
+            else if (currentState == GameState.Pause) 
+
+            UpdateState(GameState.Playing);
         }
     }
 
-    public void PauseGame()
+   public void UpdateState(GameState newState)
     {
-        Time.timeScale = 0f;
-        currentState = GameState.Paused;
+        currentState = newState;
+
+        switch (newState)
+        {
+            case GameState.MainMenu:
+                Time.timeScale = 1f;
+                break;
+            case GameState.Playing:
+                Time.timeScale = 1f;
+                break;
+            case GameState.Pause:
+                Time.timeScale = 0f; // Menghentikan game
+                break;
+            case GameState.GameOver:
+                Time.timeScale = 0f; // Menghentikan game saat kalah
+                break;
+        }
+        Debug.Log("State berubah ke: " + newState);
     }
 
-    public void GameOver()
-    {
-        Debug.Log("Game Over");
-        currentState = GameState.GameOver;
-    }
+    public void GameOver() 
+{
+    UpdateState(GameState.GameOver);
+}
 }
